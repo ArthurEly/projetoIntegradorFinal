b2cf5295c82d916d270924aa41be7d5c070ed388
Eduardo P. Gomez <eduardoprocopiogomez@Gmail.com>
2020-08-21 21:00:44 -0300

    Commit Automatico

--- PIF-Mobile/app/src/main/java/br/com/senac/pif_mobile/MainActivity.java
+++ PIF-Mobile/app/src/main/java/br/com/senac/pif_mobile/MainActivity.java
@@ -6,12 +6,14 @@ import android.app.Activity;
 import android.os.Bundle;
 
 import br.com.senac.pif_mobile.util.ActivityUtils;
+import br.com.senac.pif_mobile.util.TimeUtils;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TimeUtils.wait(next(),5000);
     }
 
     public Runnable next() {
